5a258e6bfbbe88404a6100b5f897cb64a24d2424
Audrey Andoy <audreylandoy@gmail.com>
2020-10-09 23:39:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("---Welcome to Calculator Program---")
+print()
+num1 = int(input("Provide a first number: "))
+num2 = int(input("Provide a second number: "))
+op = input("Provide an operator. Choose one: \n +, -, /, *  \n")
+
