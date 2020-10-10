484a5c12edf7837bd887c6d436e8418e121c1e54
Audrey Andoy <audreylandoy@gmail.com>
2020-10-10 00:00:53 +0000

    finished calculator

--- main.py
+++ main.py
@@ -9,20 +9,33 @@ def check_operator(op):
     global num1, num2
     if op == "+":
         # addition
+        print(f"{num1} {op} {num2}")
         addition(num1, num2)
     elif op == "-":
         # subtraction
-        pass
+        print(f"{num1} {op} {num2}")
+        subtract(num1, num2)
     elif op == "/":
         # division
-        pass
+        print(f"{num1} {op} {num2}")
+        division(num1, num2)
     elif op == "*":
         # multiplication
-        pass
+        print(f"{num1} {op} {num2}")
+        multiplication(num1, num2)
     else:
         print("hm.. that's not an operation.")
 
 def addition(n1, n2):
-    print(f"Sum is: {n1 + n2}")
+    print(f" = {n1 + n2}")
+
+def subtraction(n1, n2):
+    print(f" = {n1 - n2}")
+
+def division(n1, n2):
+    print(f" = {n1 / n2}")
+
+def multiplication(n1, n2):
+    print(f" = {n1 * n2}")
 
 check_operator(op)
